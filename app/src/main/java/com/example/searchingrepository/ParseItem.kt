0879b4737ec5a14388240a022bb185feb3400d32
BaraCuba <baranovskiy.sergey.rus@gmail.com>
2021-02-17 14:25:36 +0300

    comment

---
+++ app/src/main/java/com/example/searchingrepository/ParseItem.kt
@@ -0,0 +1,9 @@
+package com.example.searchingrepository
+
+class ParseItem (
+        var buttonSearch : String
+        var buttonSearch : String
+        var buttonSearch : String
+        var buttonSearch : String
+        var buttonSearch : String
+)
